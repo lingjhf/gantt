0891f0c71355064c0c84f885b5f47c503e03a2af
JHFeng <lingj.jhf@outlook.com>
2022-11-09 22:50:07 +0800

    修改：移除状态管理，把接收组件改为接收数据模型，分离gantt实现

--- lib/src/gantt.dart
+++ lib/src/gantt.dart
@@ -1,11 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:gantt/src/models/subject.dart';
 
 import 'enums.dart';
-import 'gantt_background.dart';
-import 'gantt_list.dart';
-import 'gantt_timeline.dart';
-import 'providers/timeline_provider.dart';
+import 'gantt_container.dart';
 
 class Gantt extends StatelessWidget {
   const Gantt({
@@ -14,19 +11,19 @@ class Gantt extends StatelessWidget {
     required this.endDate,
     this.unit = GanttDateUnit.day,
     this.scrollController,
-    this.children = const [],
+    this.data = const [],
   });
 
   final DateTime startDate;
   final DateTime endDate;
   final GanttDateUnit unit;
   final ScrollController? scrollController;
-  final List<Widget> children;
+  final List<GanttSubjectData> data;
 
   @override
   Widget build(BuildContext context) {
     var scrollController = this.scrollController ?? ScrollController();
-    return ProviderScope(child: LayoutBuilder(
+    return LayoutBuilder(
       builder: (context, constraints) {
         return SizedBox(
           width: constraints.maxWidth,
@@ -42,130 +39,14 @@ class Gantt extends StatelessWidget {
                 unit: unit,
                 viewWidth: constraints.maxWidth,
                 scrollController: scrollController,
-                children: children,
+                data: data,
               ),
             ),
           ),
         );
       },
-    ));
+    );
   }
 }
 
 // ignore: must_be_immutable
-class GanttContainer extends ConsumerWidget {
-  GanttContainer({
-    super.key,
-    required this.startDate,
-    required this.endDate,
-    required this.unit,
-    required this.viewWidth,
-    required this.scrollController,
-    this.children = const [],
-  });
-
-  final DateTime startDate;
-  final DateTime endDate;
-  final GanttDateUnit unit;
-  final double viewWidth;
-  final ScrollController scrollController;
-
-  final List<Widget> children;
-
-  GanttTimelineNotifier? timelineNotifier;
-
-  void initProviders(WidgetRef ref) {
-    WidgetsBinding.instance.addPostFrameCallback((timeStamp) {
-      timelineNotifier = ref.read(ganttTimelineProvider.notifier)!
-        ..init(
-          startDate: startDate,
-          endDate: endDate,
-          scrollController: scrollController,
-          unit: unit,
-          viewWidth: viewWidth,
-        )
-        ..setViewDates();
-      watchScrolling(timelineNotifier!);
-    });
-  }
-
-  watchScrolling(GanttTimelineNotifier notifier) {
-    scrollController.addListener(() {
-      notifier.setViewDates();
-    });
-  }
-
-  //构造时间轴
-  Widget _buildTimeline() {
-    Widget timeline;
-    switch (unit) {
-      case GanttDateUnit.day:
-        timeline = const GanttDayTimeline();
-        break;
-      case GanttDateUnit.week:
-        timeline = const GanttWeekTimeline();
-        break;
-      case GanttDateUnit.month:
-        timeline = const GanttMonthTimeline();
-        break;
-      case GanttDateUnit.quarter:
-        timeline = const GanttQuarterTimeline();
-        break;
-      case GanttDateUnit.halfYear:
-        timeline = const GanttHalfYearTimeline();
-        break;
-      case GanttDateUnit.year:
-        timeline = const GanttYearTimeline();
-        break;
-    }
-
-    return Material(
-      elevation: 2,
-      child: Container(
-        color: Colors.white,
-        height: 36,
-        child: DefaultTextStyle(
-          style: const TextStyle(
-            fontSize: 12,
-            color: Colors.black,
-            fontWeight: FontWeight.bold,
-          ),
-          child: timeline,
-        ),
-      ),
-    );
-  }
-
-  //构建甘特图体
-  Widget _buildBody() {
-    return Expanded(
-      child: Stack(
-        children: [
-          const GanttBackground(),
-          GanttList(children: children),
-        ],
-      ),
-    );
-  }
-
-  @override
-  Widget build(BuildContext context, WidgetRef ref) {
-    initProviders(ref);
-    return Consumer(
-      builder: (context, ref, child) {
-        var totalWidth = ref
-            .watch(ganttTimelineProvider.select((value) => value.totalWidth));
-        return SizedBox(
-          width: totalWidth,
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              _buildTimeline(),
-              _buildBody(),
-            ],
-          ),
-        );
-      },
-    );
-  }
-}
