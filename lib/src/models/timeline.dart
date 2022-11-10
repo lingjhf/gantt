dfa8873870642c14f5ba44cd94ae451695929073
JHFeng <lingj.jhf@outlook.com>
2022-11-10 22:20:46 +0800

    新增 timelineHighlight数据模型

--- lib/src/models/timeline.dart
+++ lib/src/models/timeline.dart
@@ -20,3 +20,35 @@ class GanttTimelineItemModel {
     );
   }
 }
+
+class GanttTimelineHighlightModel {
+  const GanttTimelineHighlightModel({
+    this.visible = false,
+    this.startDate,
+    this.endDate,
+    this.left = 0,
+    this.width = 0,
+  });
+
+  final bool visible;
+  final DateTime? startDate;
+  final DateTime? endDate;
+  final double left;
+  final double width;
+
+  GanttTimelineHighlightModel copyWith({
+    bool? visible,
+    DateTime? startDate,
+    DateTime? endDate,
+    double? left,
+    double? width,
+  }) {
+    return GanttTimelineHighlightModel(
+      visible: visible ?? this.visible,
+      startDate: startDate ?? this.startDate,
+      endDate: endDate ?? this.endDate,
+      left: left ?? this.left,
+      width: width ?? this.width,
+    );
+  }
+}
