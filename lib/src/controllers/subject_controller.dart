95c5692b3374433e6e249032b037ed59e6ce5153
JHFeng <lingj.jhf@outlook.com>
2022-11-14 01:15:37 +0800

    新增subject溢出可视宽度功能和溢出时间轴功能

--- lib/src/controllers/subject_controller.dart
+++ lib/src/controllers/subject_controller.dart
@@ -41,4 +41,39 @@ abstract class GanttSubjectController {
     startIndex = startIndex < 0 ? 0 : startIndex;
     return startIndex * dayWidth;
   }
+
+  bool leftOverflowTimeline(DateTime date) {
+    if (left < 0) {
+      timelineController.addForwardDay();
+      left = getSubjectLeft(date);
+      timelineController.updateHighlight(left: left);
+      return true;
+    }
+    return false;
+  }
+
+  bool rightOverflowTimeline(DateTime date) {
+    if (left + width > timelineController.totalWidth) {
+      timelineController.addBackDay();
+      left = getSubjectLeft(date);
+      return true;
+    }
+    return false;
+  }
+
+  void leftOverflowViewWidth() {
+    if (left < timelineController.scrollController.offset) {
+      timelineController.scrollController.jumpTo(left);
+    }
+  }
+
+  void rightOverflowViewWidth() {
+    var scrollOffset = timelineController.scrollController.offset;
+    var leftWidthSum = left + width;
+    var offsetWidthSum = scrollOffset + timelineController.viewWidth;
+    if (leftWidthSum > offsetWidthSum) {
+      timelineController.scrollController
+          .jumpTo(scrollOffset + (leftWidthSum - offsetWidthSum));
+    }
+  }
 }
