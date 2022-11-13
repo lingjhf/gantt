6651ad8b95bd479b58eabe995110eb51e437a759
JHFeng <lingj.jhf@outlook.com>
2022-11-14 01:25:51 +0800

    新增拖拽功能

--- lib/src/controllers/milestone_controller.dart
+++ lib/src/controllers/milestone_controller.dart
@@ -1,22 +1,79 @@
 import '../mixins/drag_resize.dart';
-import 'gantt_controller.dart';
+import '../utils/gantt.dart';
 import 'subject_controller.dart';
-import 'timeline_controller.dart';
 
 class GanttMilestoneController extends GanttSubjectController
     with DragResizeMixin {
   GanttMilestoneController({
-    required this.ganttController,
-    required this.timelineController,
-    this.date,
+    required super.ganttController,
+    required super.timelineController,
+    DateTime? date,
     this.finished = false,
-  });
+  }) : date = date ?? DateTime.now() {
+    left = getSubjectLeft(this.date);
+  }
 
-  GanttController ganttController;
+  @override
+  double get width => 20;
 
-  GanttTimelineController timelineController;
+  bool focused = false;
 
-  DateTime? date;
+  DateTime date;
 
   bool finished;
+
+  //显示时间轴高亮日期
+  @override
+  void visibleTimelineHighlight() {
+    timelineController.updateHighlight(
+      visible: true,
+      width: width,
+      left: left,
+      startDate: date,
+    );
+  }
+
+  //注册时间轴高亮
+  @override
+  void dragStart(double dx) {
+    super.dragStart(dx);
+    if (id != ganttController.currentSubject?.id) {
+      focused = true;
+      ganttController.setCurrentSubject(this);
+    }
+    visibleTimelineHighlight();
+  }
+
+  @override
+  void dragUpdate(double dx) {
+    super.dragUpdate(dx);
+    if (leftOverflowTimeline(date)) {
+      return;
+    }
+    if (rightOverflowTimeline(date)) {
+      return;
+    }
+    if (deltaX == 0) return;
+    if (deltaX < 0) {
+      leftOverflowViewWidth();
+    } else {
+      rightOverflowViewWidth();
+    }
+    var startIndex = getStartIndex(left, dayWidth);
+
+    date = timelineController.dates[startIndex];
+
+    timelineController.updateHighlight(
+      left: startIndex * dayWidth,
+      width: width,
+      startDate: date,
+    );
+  }
+
+  void dragEnd() {
+    if (!focused) {
+      invisibleTimelineHighlight();
+    }
+    left = timelineController.highlight.left;
+  }
 }
