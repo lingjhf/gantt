1e13d3b7e71f532be467d40c95e69325f27b82e7
JHFeng <lingj.jhf@outlook.com>
2022-11-13 23:07:47 +0800

    私有化pressedOffset

--- lib/src/mixins/drag_resize.dart
+++ lib/src/mixins/drag_resize.dart
@@ -1,19 +1,19 @@
 mixin DragResizeMixin {
   double _left = 0;
+  double _width = 0;
+  double _pressedOffset = 0;
+
+  double deltaX = 0;
 
   double get left => _left;
-
   set left(double value) {
     if (value != left) {
-      pressedOffset = pressedOffset - (value - _left);
+      _pressedOffset = _pressedOffset - (value - _left);
     }
     _left = value;
   }
 
-  double _width = 0;
-
-  double deltaX = 0;
-
+  double get width => _width;
   set width(double value) {
     if (value < 0) {
       _width = 0;
@@ -22,28 +22,25 @@ mixin DragResizeMixin {
     _width = value;
   }
 
-  double get width => _width;
-  double pressedOffset = 0;
-
   //开始拖拽
   void dragStart(double dx) {
-    pressedOffset = dx - _left;
+    _pressedOffset = dx - _left;
   }
 
   //正在拖拽
   void dragUpdate(double dx) {
-    var tempLeft = dx - pressedOffset;
+    var tempLeft = dx - _pressedOffset;
     deltaX = tempLeft - _left;
     _left = tempLeft;
   }
 
   void resizeLeftStart(double dx) {
-    pressedOffset = dx - _left;
+    _pressedOffset = dx - _left;
   }
 
   void resizeLeftUpdate(double dx) {
     var leftWidthSum = _left + width;
-    var tempLeft = dx - pressedOffset;
+    var tempLeft = dx - _pressedOffset;
     var tempWidth = leftWidthSum - tempLeft;
     if (tempLeft > leftWidthSum) {
       _left = leftWidthSum;
@@ -56,11 +53,11 @@ mixin DragResizeMixin {
 
   //开始向右调整大小
   void resizeRightStart(double dx) {
-    pressedOffset = dx - width;
+    _pressedOffset = dx - width;
   }
 
   //正在向右调整大小
   void resizeRightUpdate(double dx) {
-    width = dx - pressedOffset;
+    width = dx - _pressedOffset;
   }
 }
