8d65277e18f418aa426abbc83401f70cb1cdb922
JHFeng <lingj.jhf@outlook.com>
2022-11-15 23:53:52 +0800

    新增连接功能

--- lib/src/controllers/subject_controller.dart
+++ lib/src/controllers/subject_controller.dart
@@ -1,23 +1,38 @@
+import 'package:flutter/material.dart';
 import 'package:uuid/uuid.dart';
 
+import 'connect_controller.dart';
 import 'gantt_controller.dart';
 import 'timeline_controller.dart';
+
+typedef SubjectMap = Map<String, GanttSubjectController>;
 
 abstract class GanttSubjectController {
   GanttSubjectController({
     String? id,
     required this.ganttController,
     required this.timelineController,
+    required this.connectLineController,
   }) : id = id ?? const Uuid().v1();
   final String id;
+
+  final GlobalKey key = GlobalKey();
 
   double left = 0;
 
   double width = 0;
 
+  bool focused = false;
+
   GanttController ganttController;
 
   GanttTimelineController timelineController;
+
+  GanttConnectLineController connectLineController;
+
+  final SubjectMap _nextSubjects = {};
+
+  bool connectActive = false;
 
   double get dayWidth => timelineController.unit.dayWidth;
 
@@ -75,6 +90,74 @@ abstract class GanttSubjectController {
     if (leftWidthSum > offsetWidthSum) {
       timelineController.scrollController
           .jumpTo(scrollOffset + (leftWidthSum - offsetWidthSum));
+    }
+  }
+
+  GanttSubjectController? getNextSubject(String id) {
+    return _nextSubjects[id];
+  }
+
+  void addNextSubject(String id, GanttSubjectController subject) {
+    _nextSubjects[id] = subject;
+  }
+
+  void remoteNextSubject(String id) {
+    _nextSubjects.remove(id);
+  }
+
+  //检查是否存在循环连接
+  bool checkCircularConnect(String id) {
+    if (getNextSubject(id) != null) return true;
+    bool found = false;
+    if (_nextSubjects.isNotEmpty) {
+      _nextSubjects.forEach((key, value) {
+        if (value.checkCircularConnect(id)) {
+          focused = true;
+          return;
+        }
+      });
+    }
+    return found;
+  }
+
+  //被动连接
+  //1.判断是否有当期subject
+  //2.判断是否激活连接
+  //3.判断是否已连接
+  //4.判断是否会导致循环连接
+  void passiveConnect() {
+    if (ganttController.currentSubject == null) return;
+    if (!ganttController.currentSubject!.connectActive) return;
+    if (ganttController.currentSubject!.getNextSubject(id) != null) return;
+    if (checkCircularConnect(ganttController.currentSubject!.id)) return;
+    ganttController.currentSubject!.addNextSubject(id, this);
+    connectLineController.addByGlobKey(
+      ganttController.currentSubject!.key,
+      key,
+    );
+  }
+
+  void onConnectNext(bool active) {
+    connectActive = active;
+  }
+
+  void onFocusOut(VoidCallback callback) {
+    ganttController.on('onCurrentSubjectChange', (oldId) {
+      if (id == oldId) {
+        focused = false;
+        invisibleTimelineHighlight();
+        callback();
+      }
+    });
+  }
+
+  void onFocusIn(VoidCallback callback) {
+    if (id != ganttController.currentSubject?.id) {
+      passiveConnect();
+      focused = true;
+      ganttController.setCurrentSubject(this);
+      visibleTimelineHighlight();
+      callback();
     }
   }
 }
