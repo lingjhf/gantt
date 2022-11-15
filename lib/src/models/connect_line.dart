d8176f93a392d59c18c824452bc1a36e45c18ea0
JHFeng <lingj.jhf@outlook.com>
2022-11-15 23:48:23 +0800

    新增连接线数据模型

---
+++ lib/src/models/connect_line.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ConnectLineData {
+  const ConnectLineData({
+    required this.startOffset,
+    required this.endOffset,
+  });
+  final Offset startOffset;
+  final Offset endOffset;
+}
