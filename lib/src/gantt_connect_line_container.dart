2fe4d1c6d967dbdc02f95cdad837f8caa481f842
JHFeng <lingj.jhf@outlook.com>
2022-11-18 13:57:40 +0800

    修改连接容器组件

--- lib/src/gantt_connect_line_container.dart
+++ lib/src/gantt_connect_line_container.dart
@@ -1,8 +1,7 @@
-
-
 import 'package:flutter/material.dart';
 
 import 'controllers/connect_controller.dart';
+import 'gantt_connect_line.dart';
 
 class GanttConnectLineContainer extends StatefulWidget {
   const GanttConnectLineContainer({
@@ -10,7 +9,7 @@ class GanttConnectLineContainer extends StatefulWidget {
     required this.controller,
   });
 
-  final GanttConnectLineController controller;
+  final GanttConnectContainerController controller;
 
   @override
   State<StatefulWidget> createState() => _GanttConnectLineContainerState();
@@ -18,7 +17,19 @@ class GanttConnectLineContainer extends StatefulWidget {
 
 class _GanttConnectLineContainerState extends State<GanttConnectLineContainer> {
   @override
+  void initState() {
+    widget.controller.on('addConnectLine', (p0) => setState(() {}));
+    super.initState();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Stack();
+    return Stack(
+      key: widget.controller.key,
+      children: [
+        for (final item in widget.controller.connectLines)
+          GanttConnectLine(controller: item),
+      ],
+    );
   }
 }
