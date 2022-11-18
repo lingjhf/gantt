894b35ee332bea0b2a1c6e194383162d9808d28f
JHFeng <lingj.jhf@outlook.com>
2022-11-18 12:14:38 +0800

    新增连接线组件

--- lib/src/gantt_connect_line.dart
+++ lib/src/gantt_connect_line.dart
@@ -1,24 +1,31 @@
 import 'package:flutter/material.dart';
 
-class GanttConnectLine extends StatelessWidget {
+import 'controllers/connect_line_controller.dart';
+
+class GanttConnectLine extends StatefulWidget {
   const GanttConnectLine({
     super.key,
-    required this.startOffset,
-    required this.endOffset,
+    required this.controller,
   });
 
-  final Offset startOffset;
+  final GanttConnectLineController controller;
+  @override
+  State<StatefulWidget> createState() => GanttConnectLineState();
+}
 
-  final Offset endOffset;
+class GanttConnectLineState extends State<GanttConnectLine> {
+  @override
+  void initState() {
+    widget.controller.on('update', (p0) => setState(() {}));
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Positioned(
-      child: CustomPaint(
-        painter: GanttConnectLinePainter(
-          startOffset: startOffset,
-          endOffset: endOffset,
-        ),
+    return CustomPaint(
+      painter: GanttConnectLinePainter(
+        startOffset: widget.controller.startOffset,
+        endOffset: widget.controller.endOffset,
       ),
     );
   }
@@ -35,8 +42,29 @@ class GanttConnectLinePainter extends CustomPainter {
   final Offset endOffset;
 
   @override
-  void paint(Canvas canvas, Size size) {}
+  void paint(Canvas canvas, Size size) {
+    final paint = Paint()
+      ..style = PaintingStyle.stroke
+      ..color = Colors.blue;
+    final path = Path();
+    final double centerX = startOffset.dx + (endOffset.dx - startOffset.dx) / 2;
+    final double centerY = startOffset.dy + (endOffset.dy - startOffset.dy) / 2;
+    path.moveTo(startOffset.dx, startOffset.dy);
+    path.quadraticBezierTo(
+      centerX,
+      startOffset.dy,
+      centerX,
+      centerY,
+    );
+    path.quadraticBezierTo(
+      centerX,
+      endOffset.dy,
+      endOffset.dx,
+      endOffset.dy,
+    );
+    canvas.drawPath(path, paint);
+  }
 
   @override
-  bool shouldRepaint(covariant CustomPainter oldDelegate) => false;
+  bool shouldRepaint(covariant CustomPainter oldDelegate) => true;
 }
